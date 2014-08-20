be1526bd277ba29c94e4e55195372ed12e1748d8
jcchen <junchangchen@gmail.com>
2014-08-20 20:24:44 +0800

    update plugins

--- runplugins.sh
+++ runplugins.sh
@@ -3,6 +3,7 @@
 # This file used to keep the project's dependency plugins
 #
 
+cordova plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
 cordova plugin add https://github.com/wildabeast/BarcodeScanner
 cordova plugin add de.appplant.cordova.plugin.local-notification
 #cordova plugin add https://github.com/randdusing/BluetoothLE
